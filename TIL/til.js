65385d6fe039f3bb989fb0280a10db344c76b158
LeeDongGeun <sin4dong@12cmsv.co.kr>
2021-07-20 15:17:25 +0900

    dddddㅇㅇ

--- TIL/til.js
+++ TIL/til.js
@@ -40,12 +40,51 @@ export function getAmount(val) {
 
 
 
+const textNode = (text, target)=>{
+    if(text.length) target.push({type:'TEXT', text});
+    return '';
+};
 
 
+const elementNode = (input, cursor, text, stack, stacks)=>{
+    const char = input[cursor++];
+    let isBreak = false;
+    if(char === '<'){
+        text = textNode(text, stack.tag.children);
+        if(input[cursor++] !== '/'){
+            let name = input.substring(cursor - 1, cursor = input.indexOf('>', cursor));
+            const isClose = input[cursor] === '/';
+            if(isClose) name = name.substr(0, name.length - 1);
+            const tag = {name, type:'NODE', children:[]};
+            cursor++;
+            stack.tag.children.push(tag);
+            if(!isClose){
+                stacks.push({tag, back:stack});
+                isBreak = true;
+            }
+        }else if(stack.tag.name == input.substring(cursor, input.indexOf('>', cursor))){
+            stack = stack.back;
+        }
+    }else text += char;
+    return {cursor, text, isBreak, stack};
+};
 
-
-
-
+const parser = input=>{
+    const result = {tag:{type:'ROOT', children:[]}}, stacks = [];
+    let cursor = 0, stack = result;
+    do {
+        let text = '';
+        while (cursor < input.length) {
+            const v = elementNode(input, cursor, text, stack, stacks);
+            ({cursor, text, stack} = v);
+            if (v.isBreak) break;
+        }
+    }while(stack = stacks.pop());
+    return result.tag.children;
+};
+console.log('<div>test</div>',parser('<div>test</div>'));
+console.log('<div>test<img/></div>',parser('<div>test<img/></div>'));
+console.log('<div>test<a>aa</a>bb</div>',parser('<div>test<a>aa</a>bb</div>'));
 
 
 
